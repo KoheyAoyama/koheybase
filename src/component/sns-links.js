9c79231988586fb2c0184c2635d66fc8788ac7c2
KoheyAoyama <kou_713@hotmail.com>
2020-10-11 19:27:05 +0900

    Update: sns-link.js, support styoed-components

--- src/component/sns-links.js
+++ src/component/sns-links.js
@@ -1,26 +1,53 @@
 import React from "react"
+import styled, { css } from "styled-components"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faFacebook, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
-import styles from "./sns-links.module.scss"
 
-export default function SnsLinks() {
-    return (
-        <ul className={styles.snsLinkList}>
-            <li className={styles.snsLink}>
-                <a href="https://www.facebook.com/kohei5342">
-                    <FontAwesomeIcon className={styles.iconFacebook} icon={faFacebook} />
-                </a>
-            </li>
-            <li className={styles.snsLink}>
-                <a href="https://www.linkedin.com/in/kohei-aoyama-8b3747130/">
-                    <FontAwesomeIcon className={styles.iconLinkedin} icon={faLinkedin} />
-                </a>
-            </li>
-            <li className={styles.snsLink}>
-                <a href="https://github.com/KoheyAoyama">
-                    <FontAwesomeIcon className={styles.iconGithub} icon={faGithub} />
-                </a>
-            </li>
-        </ul>
-    )
-}
+const SnsLinks = () => (
+    <SnsList>
+        <SnsItem>
+            <a href="https://www.facebook.com/kohei5342">
+                <SnsIcon icon={faFacebook} facebook />
+            </a>
+        </SnsItem>
+        <SnsItem>
+            <a href="https://www.linkedin.com/in/kohei-aoyama-8b3747130/">
+                <SnsIcon icon={faLinkedin} linkedin />
+            </a>
+        </SnsItem>
+        <SnsItem>
+            <a href="https://github.com/KoheyAoyama">
+                <SnsIcon icon={faGithub} github />
+            </a>
+        </SnsItem>
+    </SnsList>
+)
+
+const SnsList = styled.ul`
+    display: flex;
+`
+
+const SnsItem = styled.li`
+    margin-right: 32px;
+    font-size: 48px;
+
+    &:last-child {
+        margin-right: 0;
+    }
+`
+
+const SnsIcon = styled(props => <FontAwesomeIcon {...props} />)`
+    ${props => props.facebook && css`
+        color: #1877F2;
+    `}
+
+    ${props => props.linkedin && css`
+        color: #0077B5;
+    `}
+
+    ${props => props.github && css`
+        color: #333333;
+    `}
+`
+
+export default SnsLinks
